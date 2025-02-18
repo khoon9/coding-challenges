c01393dde2f6783ce3c8c1ddf0b38054690f5e81
khoon9 <kshun08@dongguk.edu>
2025-02-18 12:59:54 +0900

    feat: BOJ 10998
    - IO

---
+++ src/skillladder/level01_io_arithmetic/P10998.java
@@ -0,0 +1,13 @@
+package skillladder.level01_io_arithmetic;
+
+import java.util.Scanner;
+
+// 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+public class P10998 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.print(a*b);
+    }
+}
