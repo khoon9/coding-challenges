46c9d282a8f7da28d0947afe5e868937b504c9b3
khoon9 <kshun08@dongguk.edu>
2025-02-27 20:22:19 +0900

    feat: BOJ 5597
    - one dimensional array

---
+++ src/skillladder/level04_one_dimensional_array/P5597.java
@@ -0,0 +1,102 @@
+package skillladder.level04_one_dimensional_array;
+
+/**
+ * 문제
+ * X대학 M교수님은 프로그래밍 수업을 맡고 있다. 교실엔 학생이 30명이 있는데, 학생 명부엔 각 학생별로 1번부터 30번까지 출석번호가 붙어 있다.
+ *
+ * 교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를 구하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 입력은 총 28줄로 각 제출자(학생)의 출석번호 n(1 ≤ n ≤ 30)가 한 줄에 하나씩 주어진다. 출석번호에 중복은 없다.
+ *
+ * 출력
+ * 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
+ *
+ * 예제 입력 1
+ * 3
+ * 1
+ * 4
+ * 5
+ * 7
+ * 9
+ * 6
+ * 10
+ * 11
+ * 12
+ * 13
+ * 14
+ * 15
+ * 16
+ * 17
+ * 18
+ * 19
+ * 20
+ * 21
+ * 22
+ * 23
+ * 24
+ * 25
+ * 26
+ * 27
+ * 28
+ * 29
+ * 30
+ * 예제 출력 1
+ * 2
+ * 8
+ * 예제 입력 2
+ * 9
+ * 30
+ * 6
+ * 12
+ * 10
+ * 20
+ * 21
+ * 11
+ * 7
+ * 5
+ * 28
+ * 4
+ * 18
+ * 29
+ * 17
+ * 19
+ * 27
+ * 13
+ * 16
+ * 26
+ * 14
+ * 23
+ * 22
+ * 15
+ * 3
+ * 1
+ * 24
+ * 25
+ * 예제 출력 2
+ * 2
+ * 8
+ */
+
+import java.util.*;
+
+public class P5597 {
+    // List 초기값 True 로 세팅, 이후 False로 변경
+    // 학생 번호 입력값에 대해 -1 이후 index로 사용 가능
+    // 출력 단계에서 index + 1 -> 학생 번호 변환 필요
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        List<Boolean> list_of_submit = new ArrayList<>(Collections.nCopies(30,true));
+
+        for (int i=0; i<28; i++){
+            int student_number = scanner.nextInt();
+            list_of_submit.set(student_number-1,false);
+        }
+
+        for (int i=0; i<30; i++){
+            if (list_of_submit.get(i)){
+                System.out.println(i+1);
+            }
+        }
+    }
+}
