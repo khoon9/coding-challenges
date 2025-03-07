b8772cb50ce70c02cd5a4db2efad455242e41960
khoon9 <kshun08@dongguk.edu>
2025-03-07 16:13:41 +0900

    feat: BOJ 24264
    - time complexity
    - 연산에 사용된 변수 및 리터럴 모두가 int 일 때, 만약 출력 값의 범위가 int 자료형 범위를 넘어갈 경우 연산 결과가 의도한 것과 다르게 나온다. 따라서, 이러한 출력 값의 범위를 사전에 예측하여, long 자료형 등을 미리 적용해 놓는 것이 필요하다.

---
+++ src/skillladder/level11_time_complexity/P24264.java
@@ -0,0 +1,48 @@
+package skillladder.level11_time_complexity;
+
+/**
+ *문제
+ * 오늘도 서준이는 알고리즘의 수행시간 수업 조교를 하고 있다. 아빠가 수업한 내용을 학생들이 잘 이해했는지 문제를 통해서 확인해보자.
+ *
+ * 입력의 크기 n이 주어지면 MenOfPassion 알고리즘 수행 시간을 예제 출력과 같은 방식으로 출력해보자.
+ *
+ * MenOfPassion 알고리즘은 다음과 같다.
+ *
+ * MenOfPassion(A[], n) {
+ *     sum <- 0;
+ *     for i <- 1 to n
+ *         for j <- 1 to n
+ *             sum <- sum + A[i] × A[j]; # 코드1
+ *     return sum;
+ * }
+ * 입력
+ * 첫째 줄에 입력의 크기 n(1 ≤ n ≤ 500,000)이 주어진다.
+ *
+ * 출력
+ * 첫째 줄에 코드1 의 수행 횟수를 출력한다.
+ *
+ * 둘째 줄에 코드1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수를 출력한다. 단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다.
+ *
+ * 예제 입력 1
+ * 7
+ * 예제 출력 1
+ * 49
+ * 2
+ */
+
+import java.util.*;
+
+public class P24264 {
+    // 입력 n: 1~500,000
+    // 출력 n*n: 1~250,000,000,000 -> Long
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        long n = scanner.nextLong();
+        sb.append(n*n).append("\n");
+        sb.append(2);
+
+        System.out.println(sb);
+    }
+}
