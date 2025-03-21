6c04a3c9119d20eb86909124c5ebcb696d3e1271
khoon9 <kshun08@dongguk.edu>
2025-03-21 21:58:19 +0900

    feat: BOJ 15649
    - backtracking
    - nPk 순열

---
+++ src/skillladder/level22_backtracking/P15649.java
@@ -0,0 +1,93 @@
+package skillladder.level22_backtracking;
+
+/**
+ *문제
+ * 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+ *
+ * 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+ * 입력
+ * 첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+ *
+ * 출력
+ * 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+ *
+ * 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+ *
+ * 예제 입력 1
+ * 3 1
+ * 예제 출력 1
+ * 1
+ * 2
+ * 3
+ * 예제 입력 2
+ * 4 2
+ * 예제 출력 2
+ * 1 2
+ * 1 3
+ * 1 4
+ * 2 1
+ * 2 3
+ * 2 4
+ * 3 1
+ * 3 2
+ * 3 4
+ * 4 1
+ * 4 2
+ * 4 3
+ * 예제 입력 3
+ * 4 4
+ * 예제 출력 3
+ * 1 2 3 4
+ * 1 2 4 3
+ * 1 3 2 4
+ * 1 3 4 2
+ * 1 4 2 3
+ * 1 4 3 2
+ * 2 1 3 4
+ * 2 1 4 3
+ * 2 3 1 4
+ * 2 3 4 1
+ * 2 4 1 3
+ * 2 4 3 1
+ * 3 1 2 4
+ * 3 1 4 2
+ * 3 2 1 4
+ * 3 2 4 1
+ * 3 4 1 2
+ * 3 4 2 1
+ * 4 1 2 3
+ * 4 1 3 2
+ * 4 2 1 3
+ * 4 2 3 1
+ * 4 3 1 2
+ * 4 3 2 1
+ */
+
+import java.util.*;
+
+public class P15649 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        function1(sb, new ArrayDeque<>(), M, N);
+
+        System.out.println(sb);
+    }
+    public static void function1(StringBuilder sb, Deque<Integer> targetDeque, int needChoice, int maxInt){
+        if (needChoice==0){
+            targetDeque.stream().forEach(target->sb.append(target+" "));
+            sb.append("\n");
+            return;
+        }
+        for (int i=1; i<=maxInt; i++){
+            if (!targetDeque.contains(i)){
+                targetDeque.addLast(i);
+                function1(sb, targetDeque, needChoice-1, maxInt);
+                targetDeque.removeLast();
+            }
+        }
+    }
+}
